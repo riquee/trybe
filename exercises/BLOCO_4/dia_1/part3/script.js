77180da5a894f8cacd666a942487a1e29d81a78c
riquee <strikedv@hotmail.com>
2021-02-21 07:22:59 -0300

    Part3 Finalizado

--- exercises/BLOCO_4/dia_1/part3/script.js
+++ exercises/BLOCO_4/dia_1/part3/script.js
@@ -1,9 +1,7 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+let base = 5
+let altura = 8
+let area = base * altura;
+console.log(area)
 
-console.log(`${patientId} ${isEnrolled} ${patientInfo} ${patientEmail}`)
+let perimetro = 2*(base + altura)
+console.log(perimetro)
