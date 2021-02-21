c919024c36102f334af4ae492c5186cbd28d124a
riquee <strikedv@hotmail.com>
2021-02-21 07:33:41 -0300

    Part 4 Finalizado

---
+++ exercises/BLOCO_4/dia_1/part4/script.js
@@ -0,0 +1,9 @@
+let nota = 79
+
+if(nota >= 80){
+    console.log('Parabéns, você foi aprovado!')
+} else if(nota < 80 && nota >= 40){
+    console.log('Você está na nossa lista de espera')
+} else if(nota < 40){
+    console.log('Você foi reprovado')
+}
