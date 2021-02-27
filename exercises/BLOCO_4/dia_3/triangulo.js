ed5d7505d555e435aafc5d8013ac579c25b403ab
riquee <strikedv@hotmail.com>
2021-02-27 19:49:44 -0300

    Exercicio 4 finalizado

--- exercises/BLOCO_4/dia_3/triangulo.js
+++ exercises/BLOCO_4/dia_3/triangulo.js
@@ -1,7 +1,25 @@
 let n = 5;
-let string = '';
-for(linha = 0; linha<=n; linha++){
+let string = "";
+let position_asterisc = n - 2;
+let maioresPosition = position_asterisc;
+let menorPosition = position_asterisc;
 
-    string += '*'
-    console.log(string)
+for (line = 1; line <= n; line++) {
+  if (line % 2 != 0) {
+
+    for (indexLine = 1; indexLine <= n; indexLine++) {
+
+      if(menorPosition <= indexLine && maioresPosition >= indexLine){
+
+        string += '*'
+      } else {
+        string += ' '
+      }
+    }
+    maioresPosition += 1
+    menorPosition -= 1
+    console.log(string);
+    string = "";
+  }
+
 }
