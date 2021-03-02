9abc28290fb1ee83f3fafc50481576e24428a539
riquee <strikedv@hotmail.com>
2021-03-02 14:15:13 -0300

    Correcao local arquivos

--- exercises/BLOCO_4/dia_4/bonus2.js
+++ exercises/BLOCO_4/dia_4/bonus2.js
@@ -1,21 +1,21 @@
 function add(num1, num2) {
 
-    let convert1 = ''; convert1 += num1
-    let convert2 = ''; convert2 += num2
-    let string = '';
-    let number = 0;
+  let convert1 = ''; convert1 += num1
+  let convert2 = ''; convert2 += num2
+  let string = '';
+  let number = 0;
 
-    for(index=0; index<convert1.length; index++){
+  for(index=0; index<convert1.length; index++){
 
 
-        number = parseInt(convert1[index]) + parseInt(convert2[index])
-        string += number
+      number = parseInt(convert1[index]) + parseInt(convert2[index])
+      string += number
 
-        
-    }
-
-    return console.log(number = parseInt(string))
-
+      
   }
+
+  return console.log(number = parseInt(string))
+
+}
 
 add(16, 18)
