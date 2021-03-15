3d540f4d28eb729703cbe1435272b228863a85bf
riquee <strikedv@hotmail.com>
2021-03-15 18:14:51 -0300

    Gerado os estados pelo js

---
+++ exercises/BLOCO_6/dia_1/exercise/script.js
@@ -0,0 +1,43 @@
+const inputState = document.querySelector("#state");
+const states = {
+  AC: "Acre",
+  AL: "Alagoas",
+  AP: "Amapá",
+  AM: "Amazonas",
+  BA: "Bahia",
+  CE: "Ceará",
+  DF: "Distrito Federal",
+  ES: "Espírito Santo",
+  GO: "Goiás",
+  MA: "Maranhão",
+  MT: "Mato Grosso",
+  MS: "Mato Grosso do Sul",
+  MG: "Minas Gerais",
+  PA: "Pará",
+  PB: "Paraíba",
+  PR: "Paraná",
+  PE: "Pernambuco",
+  PI: "Piauí",
+  RJ: "Rio de Janeiro",
+  RN: "Rio Grande do Norte",
+  RS: "Rio Grande do Sul",
+  RO: "Rondônia",
+  RR: "Roraima",
+  SC: "Santa Catarina",
+  SP: "São Paulo",
+  SE: "Sergipe",
+  TO: "Tocantins",
+};
+
+
+const addState = () => {
+  const label = document.querySelector("#state").parentNode;
+  for (state in states) {
+    const element = document.createElement("option");
+    inputState.appendChild(element);
+    element.value = state;
+    element.textContent = state;
+  }
+};
+
+addState();
