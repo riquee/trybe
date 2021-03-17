9f3c6337c837d78ad6d085428c7243dadce452f0
riquee <strikedv@hotmail.com>
2021-03-16 21:21:41 -0300

    Estilizacao finalizada

--- exercises/BLOCO_6/dia_2/bootstrap/script.js
+++ exercises/BLOCO_6/dia_2/bootstrap/script.js
@@ -38,40 +38,7 @@ const addState = () => {
     const element = document.createElement("option");
     inputState.appendChild(element);
     element.value = state;
-    element.textContent = state;
+    element.textContent = states[state];
   }
 };
 addState();
-
-const checkDate = (event) => {
-  const regex = /(\d{2})\/(\d{2})\/(\d{4})/;
-  const data = inputDate.value.match(regex);
-  if (
-    !data ||
-    data[1] > 31 ||
-    data[1] < 1 ||
-    data[2] > 12 ||
-    data[2] < 1 ||
-    data[3] < 0
-  ) {
-    event.preventDefault();
-  } else {
-    const url = location.search.slice(1).split("&");
-    url.forEach((index) => {
-      const keyValue = index.split("=");
-      const key = keyValue[0];
-      const value = keyValue[1].replace(/%2F/g,'/')
-      const li = document.createElement("li");
-      info.appendChild(li);
-      li.textContent = `${key}: ${value}`;
-    });
-    localStorage.info = info.outerHTML;
-  }
-};
-forms.addEventListener("submit", checkDate);
-
-// REFERENCIAS: https://pt.stackoverflow.com/questions/65696/como-capturar-par%C3%A2metros-passados-pela-url-usando-javascript;
-
-window.onload = () => {
-  info.innerHTML = localStorage.info;
-}
