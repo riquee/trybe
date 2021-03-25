a9223119d94a0e8f72bd44c71cd4adaca0e8a80a
riquee <strikedv@hotmail.com>
2021-03-25 13:02:10 -0300

    dia 1 finalizado

--- exercises/BLOCO_7/dia_3/part1/index.js
+++ exercises/BLOCO_7/dia_3/part1/index.js
@@ -18,12 +18,25 @@ const prizeDraw = (number, fn) => console.log(fn(number));
 
 // EX 3
 
-const correctAnswer = 'higher order function';
-const userAnswer = 'HIGHER ORDER FUNCTION';
+const correctAnswer = "higher order function";
+const userAnswer = "HIGHER ORDER FUNCTION";
 const checkResp = (fn1) => (fn2) => fn1 === fn2.toLowerCase();
-console.log(checkResp(correctAnswer)(userAnswer));
 
 // EX 4
 
-// Game Actions
+const rightAnswers = ["A", "C", "B", "D", "A", "A", "D", "A", "D", "C"];
+const studentAnswers = ["A", "N.A", "B", "D", "A", "C", "N.A", "A", "D", "B"];
 
+const repeat = (array1, array2, fn) => {
+  let count = 0;
+  for (let index = 0; index < array1.length; index += 1) {
+    count += numero = fn(array1[index], array2[index]);
+  }
+  return `TOTAL DE PONTOS: ${count}`;
+};
+
+repeat(rightAnswers, studentAnswers, (elem1, elem2) => {
+  if (elem1 == elem2) return 1;
+  if (elem2 == "N.A") return 0;
+  return -0.5;
+});
