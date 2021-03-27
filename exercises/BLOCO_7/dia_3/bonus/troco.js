45131a143e23b78992d481ddc00ec535c05bcbf2
riquee <strikedv@hotmail.com>
2021-03-27 12:27:39 -0300

    ex 1 finalizado

--- exercises/BLOCO_7/dia_3/bonus/troco.js
+++ exercises/BLOCO_7/dia_3/bonus/troco.js
@@ -1,15 +1,14 @@
-const assert = require("assert");
+const assert = require('assert');
 
 const getChange = (payable, paid) => {
   const coins = [200, 100, 50, 20, 10, 5, 2, 1];
   const change = [];
-  const { length } = coins;
   let remaining = paid - payable;
   // escreva seu código aqui...
   coins.forEach((index) => {
     if (remaining >= index) {
-      let qtd = Math.floor(remaining / index);
-      for (let count = 1; count <= qtd; count++) {
+      const qtd = Math.floor(remaining / index);
+      for (let count = 1; count <= qtd; count += 1) {
         remaining -= index;
         change.push(index);
       }
@@ -22,4 +21,4 @@ const payable = 215;
 const paid = 300;
 const expected = [50, 20, 10, 5];
 // console.log(typeof getChange(payable, paid))
-assert.deepStrictEqual(getChange(payable, paid), expected)
+assert.deepStrictEqual(getChange(payable, paid), expected);
