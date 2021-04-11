799633e9e79988aed7fee6291f12eb3d5d4bf023
riquee <strikedv@hotmail.com>
2021-04-11 17:50:49 -0300

    Exercicio 4 finalizado

---
+++ exercises/BLOCO_10/dia_1/test/myFizzBuzz.test.js
@@ -0,0 +1,19 @@
+const myFizzBuzz = require('../src/myFizzBuzz');
+
+describe('myFizzBuzz', () => {
+  it('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(45)).toBe('fizzbuzz');
+  });
+  it('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(9)).toBe('fizz');
+  });
+  it('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(25)).toBe('buzz');
+  });
+  it('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(1)).toBe(1);
+  });
+  it('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz('1')).toBeFalsy();
+  });
+});
